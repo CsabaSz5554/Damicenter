25add065da1e757ef534796439e91c3e0d470b1e
Bamboucha <40745849+CsabaSz5554@users.noreply.github.com>
2023-10-03 09:52:17 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+ // JavaScript to make the header sticky when scrolling
+ window.addEventListener("scroll", function() {
+    var header = document.getElementById("stickyHeader");
+    if (window.pageYOffset > 0) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+});
